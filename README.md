c3d8a000ec1c73687f7037f0c92c1ce8cd3ce10c
Shtihl <shtihlfd@yandex.ru>
2018-02-10 16:35:13 +0300

    Initial realisation of coursera dump script

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Coursera Dump
 
-[TODO. There will be project description]
+This module is collecting data of courses on [coursera.org](http://coursera.org) and saves it to courses.xlsx
+At your module directory
+
+# How to use
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
+
+```bash
+$ pip install -r requirements.txt # alternatively try pip3
+$ python coursera.py # alternatively try pip3
+Collecting data....
+Complete! Check courses.xlsx
+```
 
 # Project Goals
 
--- coursera.py
+++ coursera.py
@@ -1,16 +1,73 @@
+import requests
+from lxml import etree
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
 
 
-def get_courses_list():
-    pass
+def get_xml_from_sitemap():
+    response = requests.get('https://www.coursera.org/sitemap~www~courses.xml')
+    return response.content
 
 
-def get_course_info(course_slug):
-    pass
+def get_courses_list(xml, amount):
+    tree = etree.fromstring(xml)
+    courses_list = [element[0].text for element in tree]
+    return courses_list[:amount]
 
 
-def output_courses_info_to_xlsx(filepath):
-    pass
+def get_course_info(course_url):
+    dirty_html = requests.get(course_url).content
+    soup = BeautifulSoup(dirty_html, 'html.parser')
+    course_name = soup.find_all('h2')[0].get_text()
+    course_language = soup.find_all('div', 'rc-Language')[0].get_text()
+    course_start_date = soup.find_all('div', 'startdate')[0].get_text()
+    course_length = len(soup.find_all('div', 'week'))
+    try:
+        course_ratings = soup.find_all('div', 'ratings-text')[0].get_text()
+    except IndexError:
+        course_ratings = 'No ratings yet' # None
+    course_info = {
+        'course_name': course_name,
+        'course_language': course_language,
+        'course_start_date': course_start_date,
+        'course_length': course_length,
+        'course_ratings': course_ratings
+    }
+    return course_info
 
+
+def fill_courses_info_to_xlsx(courses_info, filepath='./courses.xlsx'):
+    wb = Workbook()
+    ws1 = wb.active
+    table_title = [
+        'Course name',
+        'Language',
+        'Start date',
+        'Duration (week)',
+        'Rating'
+    ]
+    ws1.append(table_title)
+    for course in courses_info:
+        course_row = [
+            course['course_name'],
+            course['course_language'],
+            course['course_start_date'],
+            course['course_length'],
+            course['course_ratings']
+        ]
+        ws1.append(course_row)
+    wb.save(filepath)
+
+
+def main():
+    print('Collecting data....')
+    course_xml = get_xml_from_sitemap()
+    course_quantity = 5
+    courses_list = get_courses_list(course_xml, course_quantity)
+    courses_info = [get_course_info(course_url) for course_url in courses_list]
+    fill_courses_info_to_xlsx(courses_info)
+    print('Complete! Check courses.xlsx')
+    
 
 if __name__ == '__main__':
-    pass
+    main()
